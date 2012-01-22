addac0afadd8b83e4f1dac82ed87bf3d4bd28048
Alexei Drummond <alexei.drummond@gmail.com>
2012-01-22 01:36:33 +0000

    Added named colors and corrected colors in serial sample tree xml

--- src/beast/app/draw/tree/NodeDecorator.java
+++ src/beast/app/draw/tree/NodeDecorator.java
@@ -12,38 +12,78 @@ import java.util.Arrays;
  */
 public class NodeDecorator extends Plugin {
 
-    public Input<String> nodeColorInput = new Input<String>("nodeColor", "The color to fill the node", "255,255,0");
+    public Input<String> nodeColorInput = new Input<String>("nodeColor", "The color to fill the node. Valid values are " + Arrays.toString(ColorName.values()) + " (default 'black'). Comma-delimited triplets of 0-255 are also permitted (e.g '128,128,0')", "black");
     public Input<Double> nodeSizeInput = new Input<Double>("nodeSize", "The size in points of the shape drawn to represent node", 5.0);
     public Input<NodeShape> nodeShapeInput = new Input<NodeShape>("nodeShape", "The shape to draw the node. Valid values are " +
             Arrays.toString(NodeShape.values()) + " (default 'circle')", NodeShape.circle, NodeShape.values());
 
     enum NodeShape {circle, square, triangle}
+
     Color nodeColor;
 
-    public NodeDecorator() {}
+    enum ColorName {
+
+        red, green, blue, yellow, orange, black, white, gray;
+
+        public Color getColor() {
+            switch (this) {
+                case red:
+                    return Color.red;
+                case green:
+                    return Color.green;
+                case blue:
+                    return Color.blue;
+                case yellow:
+                    return Color.yellow;
+                case orange:
+                    return Color.orange;
+                case white:
+                    return Color.white;
+                case gray:
+                    return Color.gray;
+                case black:
+                default:
+                    return Color.black;
+            }
+        }
+    }
+
+    public NodeDecorator() {
+    }
 
     public NodeDecorator(Node node, String colorLabel, String sizeLabel, String shapeLabel) throws Exception {
-        nodeColorInput.setValue(node.getMetaData(colorLabel),this);
-        nodeSizeInput.setValue(node.getMetaData(sizeLabel),this);
-        nodeShapeInput.setValue(node.getMetaData(shapeLabel),this);
+        nodeColorInput.setValue(node.getMetaData(colorLabel), this);
+        nodeSizeInput.setValue(node.getMetaData(sizeLabel), this);
+        nodeShapeInput.setValue(node.getMetaData(shapeLabel), this);
         initAndValidate();
     }
-    
+
     public void initAndValidate() throws Exception {
         nodeColor = getColorFromString(nodeColorInput.get());
     }
-    
+
     private Color getColorFromString(String colorString) throws Exception {
         String[] colors = colorString.split(",");
-        if (colors.length != 3)
-            throw new Exception("The color string should be three values between 0-255 separated by commas");
-        int red = Integer.parseInt(colors[0]);
-        int green = Integer.parseInt(colors[1]);
-        int blue = Integer.parseInt(colors[2]);
-        return new Color(red, green, blue);
+        if (colors.length != 3) {
+            // try to interpret as ColorName
+            return ColorName.valueOf(colorString).getColor();
+        } else {
+            int red = Integer.parseInt(colors[0]);
+            int green = Integer.parseInt(colors[1]);
+            int blue = Integer.parseInt(colors[2]);
+            return new Color(red, green, blue);
+        }
     }
-    
-    public Color getNodeColor() { return nodeColor;}
-    public double getNodeSize() { return nodeSizeInput.get();}
-    public NodeShape getNodeShape() { return nodeShapeInput.get();}
+
+    public Color getNodeColor() {
+        return nodeColor;
+    }
+
+    public double getNodeSize() {
+        return nodeSizeInput.get();
+    }
+
+    public NodeShape getNodeShape() {
+        return nodeShapeInput.get();
+    }
 }
